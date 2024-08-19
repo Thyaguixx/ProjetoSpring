7a27a260169eda83f70d3dfbaafaf1e80a9e49db
CarlosKB <fateccarlos@gmail.com>
2024-08-19 09:56:59 -0300

    spring

---
+++ src/test/java/_Desk/Thy/ThyApplicationTests.java
@@ -0,0 +1,13 @@
+package _Desk.Thy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
