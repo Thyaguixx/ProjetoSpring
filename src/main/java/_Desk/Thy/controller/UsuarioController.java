cdbcc9471866adb7c62c7ad9e3f0ff29c557dfa0
Thyago Silveira <thyagogsilveira@gmail.com>
2024-08-20 10:18:06 -0300

    Att UsuarioController

--- src/main/java/_Desk/Thy/controller/UsuarioController.java
+++ src/main/java/_Desk/Thy/controller/UsuarioController.java
@@ -1,8 +1,38 @@
 package _Desk.Thy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import _Desk.Thy.entity.Usuario;
+import _Desk.Thy.service.UsuarioService;
+
 @RestController
+@RequestMapping(value = "/usuario")
+@CrossOrigin
 public class UsuarioController {
-    
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping(value = "/allUsers")
+    public List<Usuario> todosUsuarios() {
+        return usuarioService.todosUsuarios();
+    }
+
+    @PostMapping(value = "/newUser")
+    public Usuario novoUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.novoUsuario(usuario);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Usuario buscarPeloId(@PathVariable("id") Long id) {
+        return usuarioService.buscarPeloID(id);
+    }
 }
