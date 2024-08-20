b25ef9c740045e2a20dbf89c8b37b8cd8d09efef
Thyago Silveira <thyagogsilveira@gmail.com>
2024-08-20 07:38:22 -0300

    Ajuste classe e repositório

--- src/main/java/_Desk/Thy/repository/UsuarioRepository.java
+++ src/main/java/_Desk/Thy/repository/UsuarioRepository.java
@@ -1,1 +1,8 @@
-// package 
+package _Desk.Thy.repository;
+
+import _Desk.Thy.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+    
+}
